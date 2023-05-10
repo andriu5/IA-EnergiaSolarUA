9bbdff9a87aa6d0c4f9a7d85927331e1288d9d45
andriu5 <andres.alvear5@gmail.com>
2023-05-10 14:55:45 -0400

    FIX modulo pvutils cambio de nombre de tiempo_solar_verdadero a tsv

--- pvutils/__init__.py
+++ pvutils/__init__.py
@@ -1,2 +1,2 @@
 from .pvutils import julian_day, indice_claridad_cielo, irradiancia_extra_sup_horizontal, distancia_tierra_sol, \
-                     cos_sza, declinacion_solar, angulo_horario, tiempo_solar_verdadero, ecc_del_tiempo, angulo_diario
+                     cos_sza, declinacion_solar, angulo_horario, tsv, ecc_del_tiempo, angulo_diario
